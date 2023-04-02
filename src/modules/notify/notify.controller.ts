4298c245daccfed7c57503021a6eb522c60c7118
bogdan.info <bogdan.marchukov@wilix.org>
2023-04-02 10:44:02 +0300

    create notify dto and api method send

--- src/modules/notify/notify.controller.ts
+++ src/modules/notify/notify.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { NotifyDto } from '../../dto/notify.dto';
 
-@Controller('notify')
-export class NotifyController {}
+@Controller()
+export class NotifyController {
+  @Post('send')
+  sendNotify(@Body() body: NotifyDto) {
+
+  }
+}
