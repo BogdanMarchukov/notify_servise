6551f9a7b7f84fb5c28eac85cec9ea69570ad666
bogdan.info <bogdan.marchukov@wilix.org>
2023-04-02 12:45:27 +0300

    replace config - service in not running

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -3,8 +3,8 @@ export default registerAs('database', () => ({
   type: process.env.DATABASE_TYPE || 'mysql',
   host: process.env.DATABASE_HOST || 'localhost',
   port: process.env.DATABASE_PORT || 3306,
-  username: process.env.DATABASE_USERNAME || 'root',
+  username: process.env.DATABASE_USERNAME || 'user',
   password: process.env.DATABASE_PASSWORD || 'password',
-  database: process.env.DATABASE || 'notify',
+  databaseName: process.env.DATABASE || 'notify',
   synchronize: false,
 }));
